09275bb1545c942d173475fe9e5d147482cf3e9c
Andrew Stromme <andrew.stromme@gmail.com>
2017-08-21 18:14:17 -0700

    add a simple copy script

---
+++ copy.sh
@@ -0,0 +1,6 @@
+echo cp $1.meta model/StrommeNet.meta
+cp $1.meta model/StrommeNet.meta
+echo cp $1.index model/StrommeNet.index
+cp $1.index model/StrommeNet.index
+echo cp $1.data-00000-of-00001 model/StrommeNet.data-00000-of-00001
+cp $1.data-00000-of-00001 model/StrommeNet.data-00000-of-00001
